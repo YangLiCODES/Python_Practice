4045394b2b397c73aab428320989f47d1f3420e8
Nathan <yangli.atlanta@gmail.com>
2019-02-16 22:36:13 -0500

    python

---
+++ python_practice.py
@@ -0,0 +1,15 @@
+"""
+Input. Read a line from the terminal and echo it to the user.
+"""
+print("Enter something here: ")
+user_input=input()
+print("you entered: " +user_input)
+
+"""
+Integer input. Read a line and convert it to an integer.
+"""
+
+print("Enter Integer here: ")
+user_input=input()
+print("you entered: ", int(user_input))
+
